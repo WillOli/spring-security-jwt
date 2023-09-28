dcd4c61cd662cd725b871e3175953afc0ec2e91f
WillOli <william.ika@gmail.com>
2023-09-27 23:58:39 -0300

    JWT - Json web token part 1 e 2

---
+++ src/main/java/com/willdev/springsecurityjwt/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.willdev.springsecurityjwt.controller;
+
+import com.willdev.springsecurityjwt.model.User;
+import com.willdev.springsecurityjwt.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserService service;
+    @PostMapping
+    public void postUser(@RequestBody User user){
+        service.createUser(user);
+    }
+}
